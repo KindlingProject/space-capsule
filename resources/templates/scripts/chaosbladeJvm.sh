0691e2aad7bcb8db9f4b2a37657a4768bf10bdcc
niejiangang <niejiangang@harmonycloud.cn>
2022-02-17 16:57:22 +0800

    update: Readme.md

--- resources/templates/scripts/chaosbladeJvm.sh
+++ resources/templates/scripts/chaosbladeJvm.sh
@@ -1,1 +1,1 @@
-./opt/chaosblade/blade {{ command }}  {% if classname %}--classname {{ classname }} {% endif %}{% if methodname %} --methodname {{ methodname }}{% endif %}{% if after %} --after {% endif %}
+./opt/chaosblade/blade {{ command }}  {% if classname %} --classname {{ classname }} {% endif %}{% if methodname %} --methodname {{ methodname }}{% endif %}{% if after %} --after {% endif %}
