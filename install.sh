a722cc81552d7a2ebf8c1a1c68f54606f6d1763d
niejiangang <niejiangang@harmonycloud.cn>
2022-03-10 20:50:39 +0800

    fix: install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 tar zxvf ./example/helm/helm-v3.8.0-linux-amd64.tar.gz -C ./example/helm/
 
 # install chaos-operator
-./example/helm/linux-amd64/helm install chaosblade-operator ./example/chaosblade-operator/chaosblade-operator-1.5.0-ignore-webhook-error.tar.gz --namespace chaosblade
+./example/helm/linux-amd64/helm install chaosblade-operator ./example/chaosblade-operator/chaosblade-operator-1.5.0-ignore-webhook-error.tgz --namespace chaosblade
 
 # install book-demo
 kubectl apply -f ./example/bookdemo/all-in-one.yaml
