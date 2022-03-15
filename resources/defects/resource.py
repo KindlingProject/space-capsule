dc1cbc06a26c2e8bb43c214e547da2399ca48fc8
zengzhilong <597326032@qq.com>
2022-03-15 11:33:39 +0800

    实现case5案例 (#15)
    * 实现case5案例
    
    * 删除无用代码
    
    Co-authored-by: zengzhilong <zengzhilong@harmonycloud.cn>

--- resources/defects/resource.py
+++ resources/defects/resource.py
@@ -2,7 +2,7 @@ import click
 from spacecapsule.selector import get_resources
 from spacecapsule.executor import bash_executor
 from spacecapsule.template import resource_limit, kubectl_script, k8s_resource, namespace_quota_yaml, \
-    k8s_resource_script
+    k8s_resource_script, network_policy_yaml
 
 
 def resource(namespace, resource_type, resource_name, experiment_name, container, limits, requests):
@@ -11,10 +11,14 @@ def resource(namespace, resource_type, resource_name, experiment_name, container
     bash_executor(kubectl_script, resource_limit, rollback_args, 'kubectl-rollback.sh', args)
 
 
-def namespace_quota(namespace, cpu_limits, cpu_requests, mem_requests, mem_limits,experiment_name):
+def namespace_quota(namespace, cpu_limits, cpu_requests, mem_requests, mem_limits,experiment_name,desc):
     args = locals()
     bash_executor(k8s_resource_script, namespace_quota_yaml, rollback_args2, 'k8sResource-rollback.sh', args)
 
+def network_policy(namespace, label_name, label_value,experiment_name,desc):
+    args = locals()
+    args['desc'] = 'network_policy:' + desc
+    bash_executor(k8s_resource_script, network_policy_yaml, rollback_args2, 'k8sResource-rollback.sh', args)
 
 def rollback_args(args):
     need = args['resource_type'] + '@$.metadata.annotations'
